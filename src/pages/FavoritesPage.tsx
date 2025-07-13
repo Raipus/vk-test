05d1d254e2c8a4b8c11b3f872892c039bab5805e
PC Raipus <nikita18.907@gmail.com>
2025-07-13 16:13:01 +0300

    Добавлена страница избранных фильмов

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,9 +1,29 @@
-const FavoritesPage = () => {
+import React from "react";
+import { observer } from "mobx-react-lite";
+import { useStores } from "../hooks/useStores";
+import { Box, Typography } from "@mui/material";
+import MovieCard from "../components/MovieCard";
+
+const FavoritesPage: React.FC = observer(() => {
+  const { favoriteStore } = useStores();
+
   return (
-    <div>
-      <div className="">Заглушка FavoritesPage…</div>
-    </div>
+    <Box p={3}>
+      <Typography variant="h4" mb={2}>
+        Избранное
+      </Typography>
+
+      {favoriteStore.favorites.length === 0 ? (
+        <Typography>Список избранного пуст.</Typography>
+      ) : (
+        <Box display="flex" flexWrap="wrap" gap={2}>
+          {favoriteStore.favorites.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </Box>
+      )}
+    </Box>
   );
-};
+});
 
 export default FavoritesPage;
